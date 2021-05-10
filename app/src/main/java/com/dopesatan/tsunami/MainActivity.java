53ab6d4a84d055ec0d4b8f9252810b008542f1f1
Utsanjan <59719700+utsanjan@users.noreply.github.com>
2021-05-10 20:14:49 +0530

    UI/UX-Updates

--- app/src/main/java/com/dopesatan/tsunami/MainActivity.java
+++ app/src/main/java/com/dopesatan/tsunami/MainActivity.java
@@ -2,20 +2,20 @@ package com.dopesatan.tsunami;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Html;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -24,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout relativeLayout;
     RelativeLayout overlay_relative;
     Button btnNoInternetConnection;
+
+    public class WebViewTouchListener implements View.OnTouchListener {
+        private float downX;
+
+        @Override
+        public boolean onTouch(final View v, final MotionEvent event) {
+            if (event.getPointerCount() > 1) {
+                //multi touch
+                return true;
+            }
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    downX = event.getX();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                case MotionEvent.ACTION_CANCEL:
+                case MotionEvent.ACTION_UP:
+                    // set x so that it doesn't move
+                    event.setLocation(downX, event.getY());
+                    break;
+            }
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +60,21 @@ public class MainActivity extends AppCompatActivity {
         relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         overlay_relative = (RelativeLayout) findViewById(R.id.overlay_relative);
         checkConnection();
-        mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIjoieFVMWEFJejJESWUwaFgvUCJ9");
+        mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIxoieFVMWEFJejJESWUwaFgvUCJ9");
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        mywebView.setHorizontalScrollBarEnabled(false);
+        mywebView.setOnTouchListener(new WebViewTouchListener());
+        mywebView.setHapticFeedbackEnabled(false);
+
+        mywebView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                return true;
+            }
+        });
+        mywebView.setLongClickable(false);
+
         btnNoInternetConnection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +83,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void left (View view) {
+        Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                "https://www.buymeacoffee.com/utsanjan"
+        ));
+        Toast.makeText(MainActivity.this, "Buy me a coffee :)", Toast.LENGTH_LONG).show();
+        startActivity(web);
+    }
+
+    public void right (View view) {
+        Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse(
+                "https://github.com/utsanjan/Tsunami-Bomber-Android/releases"
+        ));
+        Toast.makeText(MainActivity.this, "Checking for updates...", Toast.LENGTH_LONG).show();
+        startActivity(web);
+    }
+
     public class mywebClient extends WebViewClient{
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon){
@@ -67,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     public void checkConnection() {
         ConnectivityManager connectivityManager = (ConnectivityManager)
                 this.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -75,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
 
         if(wifi.isConnected()){
-            mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIjoieFVMWEFJejJESWUwaFgvUCJ9");
+            mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIxoieFVMWEFJejJESWUwaFgvUCJ9");
             mywebView.setVisibility(View.VISIBLE);
             relativeLayout.setVisibility(View.GONE);
             new Handler().postDelayed(new Runnable() {
@@ -83,11 +136,11 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     overlay_relative.setVisibility(View.GONE);
                 }
-            }, 14000);
-            Toast.makeText(MainActivity.this, "Welcome to Tsunami", Toast.LENGTH_LONG).show();
+            }, 20000);
+            Toast.makeText(MainActivity.this, "Loading Tsunami", Toast.LENGTH_LONG).show();
         }
         else if(mobileNetwork.isConnected()){
-            mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIjoieFVMWEFJejJESWUwaFgvUCJ9");
+            mywebView.loadUrl("https://cutt.ly/eyJ2IjoiMC4wLjEiLCJlIjoia1lyVDQwdTJheHlVMkpFVUZHMUE1aS9BM1htaUJnSkJMb09qcXQxMHRRSUNZY281ZXlFalZPZ0xkZ2JEZXk1VkNnZGMzUT09IiwiaCI6ImFzcmd2YWVzcmdldGdldCIsInMiOiI3NW0yVHJtNldrYlZLWFhIamM2ZTZBPT0iLCJpIxoieFVMWEFJejJESWUwaFgvUCJ9");
             mywebView.setVisibility(View.VISIBLE);
             relativeLayout.setVisibility(View.GONE);
             new Handler().postDelayed(new Runnable() {
@@ -95,8 +148,8 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     overlay_relative.setVisibility(View.GONE);
                 }
-            }, 14000);
-            Toast.makeText(MainActivity.this, "Welcome to Tsunami", Toast.LENGTH_LONG).show();
+            }, 20000);
+            Toast.makeText(MainActivity.this, "Loading Tsunami", Toast.LENGTH_LONG).show();
         }
         else{
             overlay_relative.setVisibility(View.VISIBLE);
